1f3231103d1761a02beea5f898672ba344cf2d0c
Chainbelow <46051767+chainbelow@users.noreply.github.com>
2020-02-04 13:50:24 -0500

    Update m_remote.sh

--- m_remote.sh
+++ m_remote.sh
@@ -3,4 +3,4 @@ echo Running Mortgage and Realty system
 echo Press Ctrl-c to quit
 echo ------------------------------------
 
-node-red --settings settings_saratoga_remote.js flows_saratoga_fin.json
+node-red --settings settings_m_remote.js flows_m_fin.json
